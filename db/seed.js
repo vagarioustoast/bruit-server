4299dd05597b56155ef2ef7a90a296b225651880
vagarioustoast <vagarioustoast@gmail.com>
2019-05-09 19:40:21 -0700

    fixes seed functionlaity and adds db connection

--- db/seed.js
+++ db/seed.js
@@ -58,4 +58,45 @@ const postList = [
   }
 ];
 
-const seedDatabase = async () => {};
+const seedDatabase = async () => {
+  try {
+    // Delete All Users
+    const deletedUsers = await User.deleteMany();
+    console.log(`Deleted ${deletedUsers.deletedCount} users.`);
+    // Delete All Posts
+    const deletedPosts = await Post.deleteMany();
+    console.log(`Deleted ${deletedPosts.deletedCount} posts.`);
+
+    // Hash User Passwords
+    for (let user in userList) {
+      const hashedPassword = bcrypt.hashSync(userList[user].password, 10);
+      userList[user].password = hashedPassword;
+    }
+    // Create New Users
+    const newUsers = await User.create(userList);
+    console.log(`Created ${newUsers.length} users.`);
+
+    // Create New Posts
+    const newPosts = await Post.create(postList);
+    console.log(`Created ${newPosts.length} posts.`);
+
+    // Associate Users/Cities/Posts
+    console.log("Associating models...");
+
+    let randomIndex = arr => Math.floor(Math.random() * arr.length);
+
+    for (let post in newPosts) {
+      console.log("Random Index = ", randomIndex(newPosts));
+      newPosts[post].userId = newUsers[randomIndex(newUsers)];
+      // Save Post
+      await newPosts[post].save();
+    }
+    console.log("Models associated.");
+    console.log("*Exiting*");
+    process.exit();
+  } catch (err) {
+    console.error(err);
+    process.exit();
+  }
+};
+seedDatabase();
--- models/index.js
+++ models/index.js
@@ -1,4 +1,11 @@
 const mongoose = require("mongoose");
+const DB_URL =
+  process.env.MONGODB_URI || "mongodb://localhost:27017/bruit-server";
+
+mongoose
+  .connect(DB_URL, { useNewUrlParser: true })
+  .then(() => console.log(`MongoDB has connected.`))
+  .catch(err => console.error(err));
 
 module.exports = {
   User: require("./User"),
